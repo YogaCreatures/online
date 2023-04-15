085c4f52387ac6b72c71b1f2c88b0877974a7a72
Hiren Ranpariya <hiren.websitedeveloper@gmail.com>
2023-04-15 09:14:29 +0530

    contact form

--- assets/js/type.js
+++ assets/js/type.js
@@ -15,6 +15,32 @@ $(function () {
   });
 });
 
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    mobile: document.getElementById("mobile").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_udueyrl";
+  const templateID = "template_mjhffwi";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("mobile").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+    })
+    .catch(err=>console.log(err));
+
+}
+
 $(function() {
     $('a[href*=#]').on('click', function(e) {
       e.preventDefault();
