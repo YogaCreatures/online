44e61d7c768bd9c934d9d896398992ad8aa80d2e
Hiren Ranpariya <hiren.websitedeveloper@gmail.com>
2023-04-15 10:51:36 +0530

    contact google form

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,21 @@
 function sendMail() {
   var params = {
     name: document.getElementById("name").value,
+    email: document.getElementById("mobile").value,
     email: document.getElementById("email").value,
+    email: document.getElementById("subject").value,
     message: document.getElementById("message").value,
   };
 
-  const serviceID = "service_udueyrl";
+  const serviceID = "service_a2tl2es";
   const templateID = "template_mjhffwi";
 
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
         document.getElementById("name").value = "";
+        document.getElementById("mobile").value = "";
         document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
         document.getElementById("message").value = "";
         console.log(res);
         alert("Your message sent successfully!!")
